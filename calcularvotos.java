72be0b3322c059aefae2ca0ef6f21df2519ba933
Matheus Santos <30882102+roxx77344@users.noreply.github.com>
2019-03-16 02:06:08 -0300

    Update calcularvotos.java

--- calcularvotos.java
+++ calcularvotos.java
@@ -1,21 +1,25 @@
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 
 public class calcularvotos{
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 	
 	double votosTotais;
 	double votosNulos;
 	double votosValidos;
 	double porcentagem;
+	String lerVotosTotais = "";
+	String lerVotosNulos = "";
 	
 	System.out.println("Digite os votos totais ");
-	Scanner sc = new Scanner (System.in);
-	votosTotais = sc.nextDouble();
+	lerVotosTotais = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+	votosTotais = Double.parseDouble(lerVotosTotais);
 	
 	System.out.println("Digite os votos nulo e em branco");
-	votosNulos = sc.nextDouble();
+	lerVotosNulos = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+	votosNulos = Double.parseDouble(lerVotosNulos);
+	
 	
 	votosValidos = votosTotais - votosNulos;
 	
