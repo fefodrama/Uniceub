db8869132101896dd1db05cd0c052083eeb3360a
Matheus Santos <30882102+roxx77344@users.noreply.github.com>
2019-03-16 02:50:39 -0300

    Update calcularVolume.java

--- calcularVolume.java
+++ calcularVolume.java
@@ -1,18 +1,32 @@
-import java.util.*;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 
 public class calcVolume{
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
-	float volume;
-	float pi = 3,14;
-	float raio;
-	float altura;
+	double volume;
+	double pi = 3.14;
+	double raio;
+	double altura;
+	double capacidade;
+	String lerRaio = "";
+	String lerAltura = "";
 	
-	System.out.println
+	System.out.print("Digite a medida do raio em metros: ");
+	lerRaio = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+	raio = Double.parseDouble(lerRaio);
 		
-		
+	System.out.print("Digite a medida da altura em metros: ");
+	lerAltura = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+	altura = Double.parseDouble(lerAltura);
 	
+	volume = pi * (raio * raio) * altura;	
+	System.out.println("O volume e: " + volume + "m³");
+	
+	capacidade = volume * 1000;
+	System.out.print("A capacidade desse volume e de: " + capacidade + " litros" );
+	
+		}
 	}
 	
-}
