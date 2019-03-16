ec167da7061c3ab5803255e60ba78689c6c6dc82
Matheus Santos <30882102+roxx77344@users.noreply.github.com>
2019-03-16 02:37:04 -0300

    Update calcularSegundos.java

--- calcularSegundos.java
+++ calcularSegundos.java
@@ -1,13 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 
 public class calcularSegundos{
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 	double segundos;
-	System.out.println("Digite os segundos");
-	Scanner sc = new Scanner (System.in);
-	segundos = sc.nextDouble();
+	String lerSegundos = "";
 	
+	System.out.println("Digite os segundos");
+	lerSegundos = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+	segundos = Double.parseDouble(lerSegundos);
+	
+
 	System.out.println(segundos + " segundos sao: " + segundos / 60 + " minutos, " + segundos / 3600 + " horas, " + segundos / 86400 + " dias" );
 		
 		
