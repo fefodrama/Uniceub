48e17c44981ffac2357c0b51fcb9712fdcdf7fe0
Matheus Santos <30882102+roxx77344@users.noreply.github.com>
2019-03-16 00:45:46 -0300

    Update calcfarenheit.java

--- calcfarenheit.java
+++ calcfarenheit.java
@@ -1,14 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 
 public class calcfarenheit{
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 	double celsius;
 	double farenheit;
+	String lerCelsius = "";
 	
 	System.out.println("Digite o grau em celsius ");
-	Scanner sc =  new Scanner (System.in);
-	celsius = sc.nextDouble();
+	lerCelsius = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+	celsius = Double.parseDouble(lerCelsius);
+	
 	
 	farenheit = (celsius * 1.8) + 32;
 	System.out.println("A temperatura em farenheit e: " + farenheit);	
