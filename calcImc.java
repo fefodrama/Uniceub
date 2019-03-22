2755dbcbb45869c1013b4f7f362a0306febcb58e
Matheus Santos <30882102+roxx77344@users.noreply.github.com>
2019-03-22 20:28:54 -0300

    Update calcImc.java

--- calcImc.java
+++ calcImc.java
@@ -1,30 +1,44 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class calcImc{
-	public static void main(String[] args) throws Exception {
-	
+public class Aula4{
+	public static void main(String args[]) throws Exception{
+		//variaveis
+		double pinga;
+		double cerveja;
+		double pastel;
+		double carneSol;
+		double refri;
+		double garçom;
+		double conta;
+		String leitor = "";
 		
-	float peso;
-	float altura;
-	float IMC;
-	String lerPeso = "";
-	String lerAltura = "";	
+		//ler quantidade
+		System.out.println("Digite a quantidade de pinga: ");
+		leitor = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+		pinga = Double.parseDouble(leitor);
 		
-	System.out.println("Digite seu peso ");
-	lerPeso = (new BufferedReader(new InputStreamReader(System.in))).readLine();
-	peso = Float.parseFloat(lerPeso);
+		System.out.println("Digite a quantidade de cerveja: ");
+		leitor = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+		cerveja = Double.parseDouble(leitor);
 		
+		System.out.println("Digite a quantidade de pastel: ");
+		leitor = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+		pastel = Double.parseDouble(leitor);
 		
+		System.out.println("Digite a quantidade de carneSol: ");
+		leitor = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+		carneSol = Double.parseDouble(leitor);
 		
-	System.out.println("Digite sua altura ");
-	lerAltura = (new BufferedReader(new InputStreamReader(System.in))).readLine();
-	altura = Float.parseFloat(lerAltura);
-	
-	IMC = peso / (altura * altura);
-	
-	System.out.println("Seu imc e " +IMC);
+		System.out.println("Digite a quantidade de refri: ");
+		leitor = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+		refri = Double.parseDouble(leitor);
+		
+		conta = (pinga * 3) + (cerveja * 8) + (pastel * 15) + (carneSol * 50) + (refri * 2);
+		garçom = (conta * 10) / 100;
+		
+		System.out.println("preço da conta = " + conta);
+		System.out.println("taxa do garçom = " + garçom);
 		
 	}
-	
 }
