9ddc7df68e27e2407973ec2961c5ca2c3c620399
Matheus Santos <30882102+roxx77344@users.noreply.github.com>
2019-03-27 21:01:35 -0300

    Update trianguloAngulo.java

--- trianguloAngulo.java
+++ trianguloAngulo.java
@@ -6,21 +6,19 @@ public class trianguloAngulo{
 	public static void main(String[] args)throws Exception{
 		
 	int angulo1, angulo2, angulo3;
-	String lerAngulo1 = "";		
-	String lerAngulo2 = "";		
-	String lerAngulo3 = "";		
+	String leitor = "";	
 		
 	System.out.print("Digite o primeiro angulo: ");	 
-	lerAngulo1 = (new BufferedReader(new InputStreamReader(System.in))).readLine();
-	angulo1 = Integer.parseInt(lerAngulo1);
+	leitor = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+	angulo1 = Integer.parseInt(leitor);
 	
 	System.out.print("Digite o segundo angulo: ");	
-	lerAngulo2 = (new BufferedReader(new InputStreamReader(System.in))).readLine();
-	angulo2 = Integer.parseInt(lerAngulo2);
+	leitor = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+	angulo2 = Integer.parseInt(leitor);
 	
 	System.out.print("Digite o terceiro angulo: ");	 
-	lerAngulo3 = (new BufferedReader(new InputStreamReader(System.in))).readLine();
-	angulo3 = Integer.parseInt(lerAngulo3);	
+	leitor = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+	angulo3 = Integer.parseInt(leitor);	
 		
 		
 	if (angulo1 == angulo2 && angulo2 == angulo3 && angulo3 == angulo1) {
