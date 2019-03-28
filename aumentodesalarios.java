69ac85ac1ac56c775db3944675037d228704a073
Matheus Santos <30882102+roxx77344@users.noreply.github.com>
2019-03-27 22:13:36 -0300

    Update aumentodesalarios.java

--- aumentodesalarios.java
+++ aumentodesalarios.java
@@ -9,8 +9,9 @@ public class aumentodesalarios{
 		double atual, novo;
 		
 	System.out.println("==========Programa de aumento de salario de classes diferentes==========\n");	
-	System.out.println("Digite o numero correspondente do seu cargo:\n 1 - escritorio\n 2 - secretario\n 3 - caixa\n 4 - gerente\n 5 - diretor\n");
+	System.out.println("Digite o numero correspondente do seu cargo:\n 1 - Gerente\n 2 - Supervisor\n 3 - Funcionario");
 	
+		System.out.println("Digite seu cargo");
 		leitor = (new BufferedReader(new InputStreamReader(System.in))).readLine();
 		cargo = Integer.parseInt(leitor);
 		
@@ -21,36 +22,27 @@ public class aumentodesalarios{
 		switch(cargo) {
 		
 		case 1:
-			novo =  (atual * 0.50) + atual;
-			System.out.println("Cargo de escritorio\n");
-			System.out.println("Aumento de 50%");
+			novo =  (atual * 0.10) + atual;
+			System.out.println("Cargo de Gerente\n");
+			System.out.println("Aumento de 10%");
 			System.out.println("Novo salario: " + novo);
 			break;
 			
 		case 2:
-			novo = (atual * 0.35) + atual;
-			System.out.println("Cargo de secretario\n");
-			System.out.println("Aumento de 35%");
+			novo = (atual * 0.15) + atual;
+			System.out.println("Cargo de supervisor\n");
+			System.out.println("Aumento de 15%");
 			System.out.println("Novo salario: " + novo);
 			break;
 			
 		case 3:
 			novo = (atual * 0.20) + atual;
-			System.out.println("Cargo de caixa\n");
+			System.out.println("Cargo de funcionario\n");
 			System.out.println("Aumento de 20%");
 			System.out.println("Novo salario: " + novo);
 			break;
 		
-		case 4:
-			novo = (atual * 0.10) + atual;
-			System.out.println("Cargo de gerente\n");
-			System.out.println("Aumento de 10%");
-			System.out.println("Novo salario: " + novo);
-			break;
-			
-		case 5:
-			System.out.println("Cargo de diretor nao recebeu aumento");
-			break;
+		
 		}				
 		
 	}
