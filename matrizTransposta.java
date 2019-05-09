8b6db1c37d4a9e40837897c45eab76a6b14f6b94
Matheus Santos <30882102+msmaiaa@users.noreply.github.com>
2019-05-08 23:24:52 -0300

    Update matrizTransposta.java

--- matrizTransposta.java
+++ matrizTransposta.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class vetor {
+public class MatrizTransposta {
     public static void main (String args[]) throws Exception{
     	//Declaração de variáveis
     	String leitor = "";
